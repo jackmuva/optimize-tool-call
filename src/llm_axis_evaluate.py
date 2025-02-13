7b30dadb90f1a964ee08023b8f03a6cd97a4d98b
Jack Mu <jackmu@umich.edu>
2025-02-13 07:51:17 -0800

    deepeval should be working but there's something wrong with the tool correcteness metric cutting off names

--- src/llm_axis_evaluate.py
+++ src/llm_axis_evaluate.py
@@ -7,7 +7,17 @@ import pandas as pd
 
 load_dotenv()
 
-def formatToolCalls(index: int, output_dict: dict):
+def completeJsonFormat(parseList:list) -> list:
+    temp = []
+    for s in parseList:
+        if len(s) > 0 and s[-1] != "}":
+            temp.append(s + "}")
+        else:
+            temp.append(s)
+    return temp
+
+
+def formatToolCalls(index: int, output_dict: dict) -> list:
     res = []
     toolMetadata = {}
     try:
@@ -16,37 +26,35 @@ def formatToolCalls(index: int, output_dict: dict):
     except Exception as e:
         print(e)
 
-    parsedToolNames = output_dict['tools'][index][2:len(output_dict['tools'][index]) - 3].split(",")
-    parsedToolOutputs = output_dict['tool_outputs'][index][2:len(output_dict['tool_outputs'][index]) - 3].split("','")
-    parsedToolInputs =  output_dict['tool_inputs'][index][2:len(output_dict['tool_inputs'][index]) - 3].split("','")
+    parsedToolNames = output_dict['tools'][index][2:len(output_dict['tools'][index]) - 2].split(",")
+    print(parsedToolNames)
+    parsedToolOutputs = output_dict['tool_outputs'][index][2:len(output_dict['tool_outputs'][index]) - 2].split("','")
+    parsedToolInputs =  output_dict['tool_inputs'][index][2:len(output_dict['tool_inputs'][index]) - 2].split("','")
 
-    temp = []
-    for s in parsedToolOutputs:
-        if s[-1] != "}":
-            temp.append(s + "}")
-        else:
-            temp.append(s)
-    parsedToolOutputs = temp
-
-    temp = []
-    for s in parsedToolInputs:
-        if s[-1] != "}":
-            print(s + "}")
-            temp.append(s + "}")
-        else:
-            temp.append(s)
-    parsedToolInputs = temp
+    parsedToolOutputs = completeJsonFormat(parsedToolOutputs) 
+    parsedToolInputs = completeJsonFormat(parsedToolInputs) 
 
     for i, toolName in enumerate(parsedToolNames):
+        if toolName == '':
+            continue
         description = ""
         for source in toolMetadata['actions']:
             actions = toolMetadata['actions'][source]
             for action in actions:
                 if action['function']['name'] == toolName:
                     description = action['function']['description']
-        toolCall = ToolCall(name=toolName, description=description, input_parameters=json.loads(parsedToolInputs[i]), output=parsedToolOutputs)
+        toolCall = ToolCall(name=toolName, description=description, input_parameters=json.loads(parsedToolInputs[i]), output=str(json.loads(parsedToolOutputs[i])))
         res.append(toolCall)
+    print(res)
     return res
+
+def formatExpectedToolCalls(index: int, output_dict: dict) -> list:
+    toolCalls = []
+    expectedTools = output_dict['tool_name'][index].split(",")
+    for name in expectedTools:
+        toolCall = ToolCall(name = name, input_parameters={})
+        toolCalls.append(toolCall)
+    return toolCalls
 
 
 
@@ -60,18 +68,18 @@ def evaluateTestCases(prefix):
             input=output_dict['prompt'][index],
             actual_output=output_dict['outputs'][index][2:len(output_dict['outputs'][index]) - 3],
             tools_called=formatToolCalls(index, output_dict),
-            expected_tools=None
+            expected_tools=formatExpectedToolCalls(index, output_dict)
         ) 
         test_cases.append(test_case)
-        print(test_cases)
         break
-    return
+    print(test_cases)
 
-    tool_correctness= ToolCorrectnessMetric()
-    task_completion = TaskCompletionMetric(model="gpt-4")
+    tool_correctness= ToolCorrectnessMetric(evaluation_params = [ToolCallParams.TOOL])
+    task_completion = TaskCompletionMetric(model="gpt-4o-mini")
 
     evaluation = evaluate(test_cases, [tool_correctness, task_completion],max_concurrent=1, ignore_errors=True, run_async=False, throttle_value=10, use_cache=True) 
     with open(f"./results/{prefix}-llm-results.json", "w") as f:
          json.dump(evaluation.model_dump(), f)
 
 evaluateTestCases("gpt")
+# evaluateTestCases("claude")
