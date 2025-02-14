1b2e1783addeb350c670bf1a1f3008fc9493e6d0
Jack Mu <jackmu@umich.edu>
2025-02-14 07:53:01 -0800

    finished analysis table for llm comparison

--- src/llm_axis_analyze.py
+++ src/llm_axis_analyze.py
@@ -1,12 +1,93 @@
 import json
 import pandas as pd
 
+
+def clean_results(input_dict: dict, dataset: dict) -> dict:
+    clean_query_results = {}
+
+    for i in range(0, len(input_dict['test_results'])):
+        clean_record = {}
+
+        clean_record['success'] = input_dict['test_results'][i]['success']
+        clean_record['input'] = input_dict['test_results'][i]['input']
+        clean_record['actual_output'] = input_dict['test_results'][i]['actual_output']
+
+        clean_record['source'] = dataset['source'][i]
+        clean_record['tool_name'] = dataset['tool_name'][i]
+        clean_record['intent'] = dataset['intent'][i]
+        clean_record['tools_used'] = dataset['tools'][i]
+        clean_record['tool_inputs'] = dataset['tool_inputs'][i]
+        clean_record['tool_outputs'] = dataset['tool_outputs'][i]
+        clean_record['error'] = dataset['error'][i]
+
+        metrics = input_dict['test_results'][i]['metrics_data']
+        metric_record = {}
+        for j in range(0, len(metrics)):
+            metric_details = {}
+
+            metric_details['threshold'] = metrics[j]['threshold']
+            metric_details['success'] = metrics[j]['success']
+            metric_details['score'] = metrics[j]['score']
+            metric_details['reason'] = metrics[j]['reason']
+
+            metric_record[metrics[j]['name']] = metric_details
+
+        clean_record['metrics'] = metric_record
+        clean_query_results[input_dict['test_results'][i]['name']] = clean_record
+    return clean_query_results
+
+
+
+def create_results_table(results: list, labels: list) -> pd.DataFrame:
+    df_dict = {}
+    df_dict['method'] = []
+    df_dict['test_case'] = []
+    df_dict['input'] = []
+    df_dict['actual_output'] = []
+    df_dict['source'] = []
+    df_dict['tool_name'] = []
+    df_dict['intent'] = []
+    df_dict['tools_used'] = []
+    df_dict['tool_outputs'] = []
+    df_dict['tool_inputs'] = []
+    df_dict['error'] = []
+    df_dict['tool_correctness_score'] = []
+    df_dict['tool_correctness_reason'] = []
+    df_dict['task_completion_score'] = []
+    df_dict['task_completion_reason'] = []
+
+    for index, res in enumerate(results):
+        for i in res.keys():
+            df_dict['test_case'].append(i)
+            df_dict['method'].append(labels[index])
+            df_dict['input'].append(res[i]['input'])
+            df_dict['actual_output'].append(res[i]['actual_output'])
+            df_dict['source'].append(res[i]['source'])
+            df_dict['tool_name'].append(res[i]['tool_name'])
+            df_dict['intent'].append(res[i]['intent'])
+            df_dict['tools_used'].append(res[i]['tools_used'])
+            df_dict['tool_outputs'].append(res[i]['tool_outputs'])
+            df_dict['tool_inputs'].append(res[i]['tool_inputs'])
+            df_dict['error'].append(res[i]['error'])
+
+            for j in res[i]['metrics'].keys():
+                if j == 'Tool Correctness':
+                    df_dict['tool_correctness_score'].append(res[i]['metrics'][j]['score'])
+                    df_dict['tool_correctness_reason'].append(res[i]['metrics'][j]['reason'])
+                elif j == 'Task Completion':
+                    df_dict['task_completion_score'].append(res[i]['metrics'][j]['score'])
+                    df_dict['task_completion_reason'].append(res[i]['metrics'][j]['reason'])
+    return pd.DataFrame.from_dict(df_dict)
+
+
 with open('./results/gpt-llm-results.json') as f:
     gpt_dict = json.load(f)
+with open('./results/claude-llm-results.json') as f:
+    claude_dict = json.load(f)
 
-print(gpt_dict.keys())
-
-for i in range(0, len(gpt_dict['test_results'])):
-    print(gpt_dict['test_results'][i].keys())
-    break
-
+gpt_data = pd.read_csv('./data/gpt-llm-axis-dataset.csv').to_dict()
+claude_data = pd.read_csv('./data/claude-llm-axis-dataset.csv').to_dict()
+gpt_res = clean_results(gpt_dict, gpt_data)
+claude_res = clean_results(claude_dict, claude_data)
+results_table = create_results_table([gpt_res, claude_res], ['gpt', 'claude'])
+results_table.to_csv('./results/llm-comparison-table.csv')
